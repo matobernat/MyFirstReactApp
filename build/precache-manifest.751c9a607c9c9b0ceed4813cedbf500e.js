80a156b39a989a4f37899e1922e7b7c5fd5ac818
matobernat <36670189+matobernat@users.noreply.github.com>
2019-08-17 17:17:18 +0200

    build

---
+++ build/precache-manifest.751c9a607c9c9b0ceed4813cedbf500e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d1512074848d5f1f7467e1006ea239",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c29ccf24164e0b7ca69c",
+    "url": "/static/css/main.50b8365d.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "c29ccf24164e0b7ca69c",
+    "url": "/static/js/main.848e1db9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
